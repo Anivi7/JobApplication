0ff288e905c22e8e2d481e0878b05e04abffe1c4
anivireddy12345 <yaraboluanivireddy@gmail.com>
2026-01-19 11:26:45 -0800

    New Application

--- src/main/java/com/example/JobApp/job/JobController.java
+++ src/main/java/com/example/JobApp/job/JobController.java
@@ -1,6 +1,8 @@
 package com.example.JobApp.job;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -16,21 +18,21 @@ public class JobController {
     }
 
     @GetMapping("/jobs")
-    public List<Job>findAll(){
-        return jobService.findAll();
+    public ResponseEntity<List<Job>>findAll(){
+        return ResponseEntity.ok(jobService.findAll());
 
     }
     @PostMapping("/jobs")
-    public String createJob(@RequestBody Job job){
+    public ResponseEntity<String> createJob(@RequestBody Job job){
         jobService.createJob(job);
-        return "Job added successfully";
+        return new ResponseEntity<>("Job added successfully", HttpStatus.OK);
     }
 
     @GetMapping("/jobs/{id}")
-    public Job getJobById(@PathVariable Long id){
+    public ResponseEntity<Job> getJobById(@PathVariable Long id){
         Job job = jobService.getJobById(id);
         if (job != null)
-            return job;
-        return new Job(1L, "TestJob", "TestJob", "20000", "20000", "Loc");
+            return new ResponseEntity<>(job, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
